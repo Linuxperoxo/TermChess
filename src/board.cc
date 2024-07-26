f1d272009017b464284d355433b3264c4d01f325
Linuxperoxo <167115676+Linuxperoxo@users.noreply.github.com>
2024-07-26 18:57:29 +0000

    Add files via upload

--- src/board.cc
+++ src/board.cc
@@ -1,23 +1,40 @@
+#include "board.h"
+#include "piece.h"
+
 #include <iostream>
 
-#include "board.h"
+#define BOARD_SIZE 8
 
-Board::Board(){
-  board.resize(8, std::vector<char>(8, 'X'));
+Board::Board() : board(BOARD_SIZE, std::vector<Piece*>(BOARD_SIZE, nullptr)){};
+
+Board::~Board(){
+  for(const auto& row : board){
+    for(const auto& piece : row ){
+      delete piece;
+    }
+  }
 }
 
 void Board::printBoard(){
   for(const auto& row : board){
     std::cout << "+---+---+---+---+---+---+---+---+" << '\n';
-    for(const char& col : row){
-      std::cout << '|' << ' ' << col << ' ';
+    for(const auto& piece : row){
+      if(piece != nullptr){
+        std::cout << '|' << ' ' << piece->getType() << ' ';
+      } else {
+        std::cout << '|' << ' ' << ' ' << ' ';
+      }
     }
     std::cout << '|' << '\n';
   }
   std::cout << "+---+---+---+---+---+---+---+---+" << '\n';
 }
 
-void Board::put_in_board(char type, int subRow, int subCol, int newRow, int newCol){
-  board[subRow][subCol] = 'X';
-  board[newRow][newCol] = type;
-};
+void Board::placeObject(Piece* object){ 
+  if(board[object->getRow()][object->getCol()] != nullptr){
+    std::cout << "Colision piece detected" << '\n';
+    delete board[object->getRow()][object->getCol()];
+  }
+
+  board[object->getRow()][object->getCol()] = object;
+}
--- src/piece.h
+++ src/piece.h
@@ -4,15 +4,24 @@
 class Piece{
 protected:
   char type;
+  int color;
   int row;
   int col;
   int old_row;
   int old_col;
 
-  Piece(int init_row, int init_col);
-  virtual ~Piece() = default;
+  Piece(int c, int init_row, int init_col) : color(c), row(init_row), col(init_col){};
   
   virtual int move(int newRow, int newCol) = 0;
+
+public:
+   virtual ~Piece() = default;
+
+  char getType() const { return type; }
+  int getRow() const { return row; }
+  int getCol() const { return col; }
+  int getOldRow() const { return old_row; }
+  int getOldCol() const { return old_col; }
 };
 
 #endif
