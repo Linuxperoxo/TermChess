08776ce9dce18235a331886b4e313ca376b4b49f
Linuxperoxo <167115676+Linuxperoxo@users.noreply.github.com>
2024-07-28 15:53:20 +0000

    Add files via upload

--- src/pawn.cc
+++ src/pawn.cc
@@ -15,14 +15,16 @@
 
 //==========================================================| MACROS
 
-#define MAX_BOARD 8
-#define MIN_BOARD 1
+#define MAX_BOARD 7
+#define MIN_BOARD 0
 
 //==========================================================| CLASS FUNCTIONS
 
 Pawn::Pawn(int c, int row, int col) : Piece(c, row, col){
   // Definido a cor da peça
-  this->type = ( c == 0 ? 'P' : 'p'); 
+  this->type = ( c == 0 ? 'P' : 'p');
+  // Falando que é o primeiro movimento da peça
+  this->first_move = true;
 }
 
 void Pawn::movePiece(int row, int col){
@@ -33,27 +35,66 @@ void Pawn::movePiece(int row, int col){
   // Armazenando posição atual
   this->row = row;
   this->col = col;
+
+  first_move = (first_move == true ? false : false);
 }
 
-bool Pawn::checkIsValidMovement(int row, std::vector<std::vector<Piece*>> board){
+bool Pawn::checkIsValidMovement(int row, int col, std::vector<std::vector<Piece*>> board){
   
+  // Como matriz começa no 0 aqui nos tiramos 1 de cada entranda para ficar no tamanho certo da matriz
+  --row;
+  --col;
+
   // Verificando se o movimento é válido de acordo com o tamanho do board
   if(row > MAX_BOARD || row < MIN_BOARD){
     std::cerr << "Invalid movement!\n";
     return false;
   }
 
-  // Verificando se o peão está voltando uma casa
+  // Verificando se o peão branco está voltando uma casa
   if(this->color == 0){
     if(row < this->row){
       std::cerr << "Invalid movement!\n";
       return false;
     }
+
+    // Verificando se é o primeiro movimento da peça branca
+    if(!first_move){
+      // Caso não seja o primeiro movimento aqui ele verifica se o peão branco está andando mais que uma casa
+      if(row > (this->row + 1)){
+        std::cerr << "Invalid movement!\n";
+        return false;
+      }
+      // Caso seja o primeiro movimento ele verifica se o peão branco está andando mais de duas casas
+    } else if(row > (this->row + 2)){
+      std::cerr << "Invalid movement!\n";
+      return false;
+    }
   } else {
+    // Verificando se o peão preto está voltando uma casa
     if(row > this->row){
       std::cerr << "Invalid movement!\n";
       return false;
     }
+
+    // Verificando se é o primeiro movimento da peça preta
+    if(!first_move){
+      // Caso não seja o primeiro movimento aqui ele verifica se o peão preto está andando mais de uma casa
+      if(row < (this->row - 1)){
+        std::cerr << "Invalid movement!\n";
+        return false;
+      }
+      // Caso seja o primeiro movimento aqui ele verifica se o peão preto está andando mais que duas casas
+    } else if(row < (this->row - 2)){
+      std::cerr << "Invalid movement!\n";
+      return false;
+    }
+  }
+
+  // Verificando se a casa a frente tem alguma peça
+  if(board[row][this->col] != nullptr){
+    std::cerr << "Invalid movement!\n";
+    return false;
   }
 
   // Caso seja válido a peça vai andar 
