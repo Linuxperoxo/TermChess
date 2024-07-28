66fe15c96d24544b0637b85704fcff74ca0e7f78
Linuxperoxo <167115676+Linuxperoxo@users.noreply.github.com>
2024-07-28 15:53:54 +0000

    Add files via upload

--- src/chess.cc
+++ src/chess.cc
@@ -3,17 +3,27 @@
 
 int main(){
   Board* board = new Board;
-  Pawn* pawn1 = new Pawn(1,1,0);
+
+  Pawn* pawn1 = new Pawn(0,0,0);
+  Pawn* pawn2 = new Pawn(1,7,0);
 
   board->putPiece(pawn1);
+  board->putPiece(pawn2);
   board->printBoard();
   
-  pawn1->checkIsValidMovement(0, board->getBoard());
-
+  pawn1->checkIsValidMovement(2, 1, board->getBoard());
+  pawn2->checkIsValidMovement(7, 1, board->getBoard());
+  
   board->putPiece(pawn1);
+  board->putPiece(pawn2);
+  board->printBoard();
+  
+  pawn2->checkIsValidMovement(6, 1, board->getBoard());
+
+  board->putPiece(pawn2);
   board->printBoard();
 
   delete board;
-
+  
   return 0;
 }
