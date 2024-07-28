f1a57b86c1b74302f294f260232748b6b4816f16
Linuxperoxo <167115676+Linuxperoxo@users.noreply.github.com>
2024-07-28 15:53:37 +0000

    Add files via upload

--- src/pawn.h
+++ src/pawn.h
@@ -15,14 +15,19 @@
 
 class Pawn : public Piece{
 private:
+  
+  // Primeiro movimento da peça
+  bool first_move;
+
   // Pegando função virtual de Piece
   void movePiece(int row, int col) override; 
 public:
+
   // Construtor
   Pawn(int c, int row, int col);
 
   // Função que verifica se o movimento é válido
-  bool checkIsValidMovement(int row, std::vector<std::vector<Piece*>> board);
+  bool checkIsValidMovement(int row, int col, std::vector<std::vector<Piece*>> board);
 };
 
 #endif
