e6bec54aad7ec7501d17bcaeff5f9245d4e1935d
Linuxperoxo <167115676+Linuxperoxo@users.noreply.github.com>
2024-07-28 01:03:20 +0000

    Add files via upload

--- src/pawn.h
+++ src/pawn.h
@@ -1,15 +1,31 @@
+//==========================================|
+//   FILE: pawn.h                           |
+//   AUTHOR: Linuxperoxo                    |
+//   COPYRIGHT: (c) 2024 per Linuxperoxo.   |
+//==========================================/
+
 #ifndef PAWN_H
 #define PAWN_H
 
+//==========================================================| .H
+
 #include "piece.h"
+
+//==========================================================| CLASS DEFINITION
 
 class Pawn : public Piece{
 private:
+  // Pegando função virtual de Piece
   void movePiece(int row, int col) override; 
 public:
+  // Construtor
   Pawn(int c, int row, int col);
 
-  bool checkIsValidMovement(int row, int col, std::vector<std::vector<Piece*>> board) override;
+  // Destrutor
+  ~Pawn() = default;
+
+  // Função que verifica se o movimento é válido
+  bool checkIsValidMovement(int row, std::vector<std::vector<Piece*>> board);
 };
 
 #endif
--- src/piece.h
+++ src/piece.h
@@ -1,24 +1,50 @@
+//==========================================|
+//   FILE: piece.h                          |
+//   AUTHOR: Linuxperoxo                    |
+//   COPYRIGHT: (c) 2024 per Linuxperoxo.   |
+//==========================================/
+
 #ifndef PIECE_H
 #define PIECE_H
 
+//==========================================================| LIBS
+
 #include <vector>
+
+//==========================================================| CLASS DEFINITION
 
 class Piece{
 protected:
+
+  // Inicial da peça 
   char type;
+
+  // Cor 
   int color;
+
+  // Posição atual no eixo Y
   int row;
+
+  // Posição atual no eixo X
   int col;
+
+  // Posição Y anterior a atual
   int old_row;
+
+  // Posição X anterior a atual
   int old_col;
 
+  // Construtor 
   Piece(int c, int row, int col) : color(c), row(row), col(col), old_row(row), old_col(col){};
 
-  virtual bool checkIsValidMovement(int row, int col, std::vector<std::vector<Piece*>> board) = 0;
+  // Função virtual pura 
   virtual void movePiece(int row, int col) = 0;
 public:
+
+  // Destrutor virtual
   virtual ~Piece() = default;
 
+  // Funções get para encapsular a classe 
   char getType() const { return type; }
   int getColor() const { return color; }
   int getRow() const { return row; }
